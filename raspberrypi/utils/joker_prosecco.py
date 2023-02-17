aa68f725b1ae51487a6bce6c5dda7516135b81f7
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-02-17 14:56:37 +0100

    Fix typos and formatting

--- raspberrypi/utils/joker_prosecco.py
+++ raspberrypi/utils/joker_prosecco.py
@@ -8,17 +8,17 @@ def give_random_processo():
     - Everyday choose a random ID
     - Everyday choose a random number between 0-100 if number under 6 then give a Joker processo
     """
-    ID_list = list(db.reference("/Users").get().keys())
-    nr_participants = len(ID_list)
-    the_choosen_number = random.randint(0, nr_participants - 1)
-    id = ID_list[the_choosen_number]
+    id_list = list(db.reference("/Users").get().keys())
+    number_of_participants = len(id_list)
+    the_chosen_number = random.randint(0, number_of_participants - 1)
+    id = id_list[the_chosen_number]
     split_id = id.split(":")
-    the_choosen_one = split_id[1]
+    the_chosen_one = split_id[1]
 
     joker_proseco = random.randint(0, 100) <= 1
     if joker_proseco:
-        increment_prosecco(the_choosen_one)
-        print(str(the_choosen_one), " got a Joker Prosecco!!!")
+        increment_prosecco(the_chosen_one)
+        print(str(the_chosen_one), " got a Joker Prosecco!!!")
 
 
 if __name__ == "__main__":
