ad18c39b3d475ddf4910caccbe371dc8470fe831
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-02-17 09:38:35 +0100

    Move while loop to main, pass on variables

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from firebase.firebase_setup import firebase_setup
 from firebase.read_rfid import read_rfid
+from firebase.check_time import check_if_late
 
 
 def main():
     firebase_setup()
-    read_rfid()
+
+    while True:
+        id, arrival_time, departure_time = read_rfid()
+        check_if_late(id, arrival_time, departure_time)
 
 
 if __name__ == "__main__":
