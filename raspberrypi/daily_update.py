6c5763f0ed20114b909410563cefb6776e3526bc
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-02-18 19:38:41 +0100

    FIx no show logic maybe

--- raspberrypi/daily_update.py
+++ raspberrypi/daily_update.py
@@ -1,4 +1,4 @@
-# import pyrebase
+import pyrebase
 from datetime import datetime
 from utils.firebase_setup import firebase_setup
 from utils.check_time import increment_prosecco
@@ -11,32 +11,36 @@ def daily_update():
     firebase = firebase_setup()
     db = firebase.database()
     # Get all users
-    users_ids = db.child("Users").get().val()
+    user_ids = db.child("Users").get().val()
     today = datetime.today()
     current_date = today.strftime("%Y-%m-%d")
-
-    for user_id in users_ids:
-        # Check if the user has a registered absence_dates today
-        absence_dates = (
-            db.child("Users").child(user_id).child("absence_dates").get().val()
-        )
-        # Continue if there is an absence_dates with key current_date
-        if (absence_dates is not None) and (current_date in absence_dates):
-            continue
-
-        # Check if the user has a registered arrival_time today
-        arrival_time = (
-            db.child("Users")
-            .child(user_id)
-            .child("arrival_times")
-            .child(current_date)
-            .get()
-            .val()
-        )
-        if arrival_time is None:
-            # If the user has not arrived, increment prosecco_marks by two
-            NO_SHOW_PENALTY = 2
+    NO_SHOW_PENALTY = 2
+    for user_id in user_ids:
+        if user_did_not_show(user_id, db, current_date):
             increment_prosecco(user_id, db, penalty_points=NO_SHOW_PENALTY)
+
+
+def user_did_not_show(user_id: str, db: pyrebase, current_date: str):
+    # Check if the user has a registered absence_dates today
+    absence_dates = (
+        db.child("Users").child(user_id).child("absence_dates").get().val()
+    )
+    # Continue if there is a registered absence_dates today
+    if (absence_dates is not None) and (current_date in absence_dates):
+        return False
+
+    # Check if the user has a registered arrival_time today
+    arrival_time = (
+        db.child("Users")
+        .child(user_id)
+        .child("arrival_times")
+        .child(current_date)
+        .get()
+        .val()
+    )
+    if arrival_time is None:
+        return True
+    return False
 
 
 if __name__ == "__main__":
