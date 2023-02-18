cb601376c3a8fc8cc40b951fc4a83e82cc2c232d
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-02-18 17:46:03 +0100

    Make absolute path for the json file

--- raspberrypi/utils/firebase_setup.py
+++ raspberrypi/utils/firebase_setup.py
@@ -1,13 +1,20 @@
 import pyrebase
+import os
 
 
 def firebase_setup():
+    # Make an absolute path to the json file
+    path = os.path.dirname(os.path.abspath(__file__))
+    json_path = os.path.join(
+        path, "nettsidev1-76f5e-firebase-adminsdk-w8ekw-d84ea01cad.json"
+    )
+
     config = {
         "apiKey": "AIzaSyC1VHPwn62vcyx_THa8COonJfY1lWEi-l0",
         "authDomain": "nettsidev1-76f5e.firebaseapp.com",
         "databaseURL": "https://nettsidev1-76f5e.firebaseio.com",
         "storageBucket": "nettsidev1-76f5e.appspot.com",
-        "serviceAccount": "utils/nettsidev1-76f5e-firebase-adminsdk-w8ekw-d84ea01cad.json",
+        "serviceAccount": json_path,
     }
     firebase = pyrebase.initialize_app(config)
     return firebase
