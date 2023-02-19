f60d52fbfe2de8ab9490ae62911a4a356a9c64df
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-02-19 12:54:05 +0100

    Fix ID format in passed to increment_prosecco()

--- raspberrypi/utils/joker_prosecco.py
+++ raspberrypi/utils/joker_prosecco.py
@@ -10,14 +10,13 @@ def give_random_processo(firebase: pyrebase):
     - Everyday choose a random number between 0-100 if number under 6 then give a Joker processo
     """
     db = firebase.database()
+
     id_list = list(db.child("Users").get().val().keys())
     number_of_participants = len(id_list)
     the_chosen_number = random.randint(0, number_of_participants - 1)
-    id = id_list[the_chosen_number]
-    split_id = id.split(":")
-    the_chosen_one = split_id[1]
+    the_chosen_one = id_list[the_chosen_number]
 
-    joker_proseco = random.randint(0, 100) <= 1
+    joker_proseco = random.randint(0, 100) >= 1
     if joker_proseco:
         increment_prosecco(the_chosen_one, db, JOKER_PROSECCO_PENALTY)
         print(str(the_chosen_one), " got a Joker Prosecco!!!")
