f2766e51ca0d3e0b557ca6691361d0d2759b5bf2
KatinkaM <66777434+KatinkaM@users.noreply.github.com>
2023-02-21 10:19:17 +0100

    did some style changes to joker prosecco

--- webpage/website_front_end/scripts/create_joker_table.js
+++ webpage/website_front_end/scripts/create_joker_table.js
@@ -10,36 +10,46 @@ var joker = database.ref('Users');
 joker.on('value', function(snapshot) {
     const container = document.getElementById("joker_name");
     container.innerHTML = "";
+    const title = document.createElement("div");
+    title.innerHTML = "<h5><b>Joker Prosecco</b></h5>";
+    
+    container.appendChild(title);
+    container.style.display = "none";
 
     snapshot.forEach(function(userSnapshot) {
     var user = userSnapshot.val();
-    nr_proseccos = user['joker_prosecco']
 
+    
+
+    nr_proseccos = user['joker_prosecco']
+    joker_tab = []
 
     // Append the p element to the container div
     console.log(nr_proseccos)
     if(nr_proseccos>0){
+        container.style.display = "block";
         const user_name = document.createElement("span");
-        user_name.textContent = user['name'];
-        container.appendChild(user_name);
-
-        const text = document.createElement("span");
-        text.textContent = " : ";
-        container.appendChild(text);
         nr_joker_prosecco = user["joker_prosecco"]
+        user_name.textContent = user['name'] + " : "; 
+        container.appendChild(user_name);
+        
         for (let i = 0; i < nr_joker_prosecco; i++) {
             const image = document.createElement("img");
             image.setAttribute("src", "./images/goblin.jpg");
-            image.setAttribute("width", "10");
+            image.setAttribute("width", "12");
             container.appendChild(image);
             // Code to be executed inside the loop
         };
 
 
         const br = document.createElement("br");
-
+        const ar = document.createElement("br");
+        const t = document.createElement("span");
+        t.textContent = "   ";
         // Append the line break element to the container div
         container.appendChild(br);
+        container.appendChild(t);
+        container.appendChild(ar);
         }
     
     });
