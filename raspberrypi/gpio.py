264199bd4cc3dcd60b5c77e9aacabfcd9b7861f0
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-02-22 15:48:58 +0100

    Also use the funtion

--- raspberrypi/gpio.py
+++ raspberrypi/gpio.py
@@ -11,17 +11,10 @@ from . import utils
 if os.name == 'posix':
     if sys.platform.startswith('linux'):
         from . import _pigpio
-    elif sys.platform == 'darwin':
-        from . import _rpi_gpio
     else:
-        raise RuntimeError('Unsupported platform')
-elif os.name == 'nt':
-    from . import _win_gpio
-else:
-    raise RuntimeError('Unsupported platform')
+        print("Bing bong")
 
 
 def turn_on_GPIO0():
     """Turn on the GPIO0 LED."""
     _pigpio.turn_on_GPIO0()
-
