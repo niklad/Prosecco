a5737ecd71e60c4c45540ee0f97c4efe2118251e
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-02-22 16:02:44 +0100

    Try new pin

--- raspberrypi/gpio.py
+++ raspberrypi/gpio.py
@@ -7,7 +7,7 @@ import RPi.GPIO as GPIO
 
 def turn_on_GPIO0():
     """Turn on the GPIO0 LED."""
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(0, GPIO.OUT)
-    GPIO.output(0, GPIO.HIGH)
+    GPIO.setmode(GPIO.BCM)  # set the pin numbering scheme to BCM
+    GPIO.setup(17, GPIO.OUT)  # set the pin as an output pin
+    GPIO.output(17, GPIO.HIGH)
     print("GPIO0 turned on.")
