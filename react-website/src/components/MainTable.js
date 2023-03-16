f025fe7b82973f12a27f085f92caece0a55ce047
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-03-16 18:25:19 +0100

    Add tomorrow's meeting time in table

--- react-website/src/components/MainTable.js
+++ react-website/src/components/MainTable.js
@@ -2,47 +2,53 @@ import React, { useEffect, useState } from "react";
 import { database } from "./FirebaseConfig";
 import { onValue, ref } from "firebase/database";
 import GetTodaysDate from "./GetTodaysDate";
+import GetTomorrowsDate from "./GetTomorrowsDate";
+
 
 function MainTable() {
-  const [tableData, setTableData] = useState([]);
+    const [tableData, setTableData] = useState([]);
 
-  useEffect(() => {
-    const usersRef = ref(database, "Users");
+    useEffect(() => {
+        const usersRef = ref(database, "Users");
 
-    onValue(usersRef, (snapshot) => {
-      const data = [];
+        onValue(usersRef, (snapshot) => {
+            const data = [];
 
-      snapshot.forEach((childSnapshot) => {
-        const childData = childSnapshot.val();
-        data.push(childData);
-      });
+            snapshot.forEach((childSnapshot) => {
+                const childData = childSnapshot.val();
+                data.push(childData);
+            });
 
-      setTableData(data);
-    });
-  }, []);
+            setTableData(data);
+        });
+    }, []);
 
-  const today = GetTodaysDate();
+    const todaysDate = GetTodaysDate();
+    const tomorrowsDate = GetTomorrowsDate();
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Navn</th>
-          <th>Antalll proseccosetreker</th>
-          <th>Dagens møtetid</th>
-        </tr>
-      </thead>
-      <tbody>
-        {tableData.map((user) => (
-          <tr key={user.name}>
-            <td>{user.name}</td>
-            <td>{user.prosecco_marks}</td>
-            <td>{user.meeting_times[today]}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Navn</th>
+                    <th>Antalll proseccosetreker</th>
+                    <th>Dagens møtetid</th>
+                    <th>Morgendagens møtetid</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tableData.map((user) => (
+                    <tr key={user.name}>
+                        <td>{user.name}</td>
+                        <td>{user.prosecco_marks}</td>
+                        <td>{user.meeting_times[todaysDate]}</td>
+                        <td>{user.meeting_times[tomorrowsDate]}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
 }
 
 export default MainTable;
