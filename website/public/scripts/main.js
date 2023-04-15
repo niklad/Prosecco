1487d583f4747d2b2c1402917ca757315ecfc6ff
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-04-15 18:04:00 +0200

    More transparency

--- website/public/scripts/main.js
+++ website/public/scripts/main.js
@@ -181,7 +181,7 @@ db_ref_object.on('value', function (snapshot) {
         // Update status to include "kom for sent" if user was late
         if (presence_status_before_time == 'Kom på sal kl. ' || presence_status == 'Har dratt hjem') {
             if (user['arrival_times'][date_today] > (todays_meeting_time + ':59')) {
-                presence_status = '<span style="color: #FF0000; font-weight: bold;">' + presence_status + '</span>';
+                presence_status = '<span style="color: #000000; font-weight: bold;">' + presence_status + '</span>';
             }
         }
 
