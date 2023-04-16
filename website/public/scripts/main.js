4342df7fe379fd61fe8c7d866ea4aa1085a7ce76
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-04-16 11:35:47 +0200

    Fix clock for mobile

--- website/public/scripts/main.js
+++ website/public/scripts/main.js
@@ -326,10 +326,10 @@ function get_standard_time(user, date) {
 
 
 function updateClock() {
-    var now = new Date();
-    var hours = now.getHours().toString().padStart(2, '0');
-    var minutes = now.getMinutes().toString().padStart(2, '0');
-    var seconds = now.getSeconds().toString().padStart(2, '0');
+    let now = new Date();
+    let hours = now.getHours().toString().padStart(2, '0') + ' : ';
+    let minutes = now.getMinutes().toString().padStart(2, '0') + ' : ';
+    let seconds = now.getSeconds().toString().padStart(2, '0');
     document.getElementById('hours').textContent = hours;
     document.getElementById('minutes').textContent = minutes;
     document.getElementById('seconds').textContent = seconds;
