1ab721445fb995a99e3ca1550b7cd9b24bcd7037
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-04-16 11:24:40 +0200

    Add clock to top of home page

--- website/public/scripts/main.js
+++ website/public/scripts/main.js
@@ -323,3 +323,16 @@ function get_standard_time(user, date) {
 
     return standard_time;
 }
+
+
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours().toString().padStart(2, '0');
+    var minutes = now.getMinutes().toString().padStart(2, '0');
+    var seconds = now.getSeconds().toString().padStart(2, '0');
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('minutes').textContent = minutes;
+    document.getElementById('seconds').textContent = seconds;
+}
+
+setInterval(updateClock, 1000);
