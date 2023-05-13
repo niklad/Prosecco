90ff3611774b7ab71ac80ef0af4a9fbd21d0b9a8
Niklas Strømsnes <niklast@stud.ntnu.no>
2023-05-13 10:28:14 +0200

    Format gpio.py with black

--- raspberrypi/utils/gpio.py
+++ raspberrypi/utils/gpio.py
@@ -1,13 +1,11 @@
 """Functions for controlling the GPIO pins on the Raspberry Pi."""
 import RPi.GPIO as GPIO
 import time
-from utils.constants import (NUMBER_OF_LEDS)
+from utils.constants import NUMBER_OF_LEDS
 import board
 import neopixel
 
 import colorsys
-
-
 
 
 def configure_GPIO_pins():
@@ -35,16 +33,28 @@ def blink_LEDs(color: tuple, number_of_blinks: int, delay: float):
         turn_off_LEDs()
         time.sleep(delay)
 
+
 def blink_rainbow_LEDs(colors: list, number_of_blinks: int, delay: float):
     """Blink the GPIO0 LED in a rainbow color sequence."""
     for _ in range(number_of_blinks):
         for i in range(len(colors)):
             start_color = colors[i]
-            end_color = colors[(i+1) % len(colors)] # wrap around to the first color at the end
-            for j in range(50): # generate 100 intermediate colors between start and end
-                r, g, b = [int(255*x) for x in colorsys.hsv_to_rgb(j/100, 1, 1)] # convert HSV to RGB
-                color = tuple([start_color[k] + int((end_color[k] - start_color[k]) * j/100) for k in range(3)])
+            end_color = colors[
+                (i + 1) % len(colors)
+            ]  # wrap around to the first color at the end
+            for j in range(
+                50
+            ):  # generate 100 intermediate colors between start and end
+                r, g, b = [
+                    int(255 * x) for x in colorsys.hsv_to_rgb(j / 100, 1, 1)
+                ]  # convert HSV to RGB
+                color = tuple(
+                    [
+                        start_color[k] + int((end_color[k] - start_color[k]) * j / 100)
+                        for k in range(3)
+                    ]
+                )
                 turn_on_LEDs(color)
-                time.sleep(delay/100)
+                time.sleep(delay / 100)
                 turn_off_LEDs()
-                time.sleep(delay/100)
+                time.sleep(delay / 100)
